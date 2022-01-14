44b79807b3caf03f5723db2b3fb51fc1c11fef43
Juliet-Ol <agutjuliet@gmail.com>
2022-01-15 02:56:24 +0300

    create js file

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+
+
+
+function pizza ("size", "crust", "topping", "flavour", "quantity"){
+    this.size
+    this.crust
+    this.toppings
+    this.flavour
+    this.quantity
+}
+
+
+
+
+var small = { name: "small", price: 800 };
+var medium = { name: "medium", price: 1000 };
+var large = { name: "large", price: 1200 };
+
+var pizzaSize  = { sizes: [small, medium, large] };
+
+
+
+
+
+
+// var iPhone = { name: "iPhone", price: 699 };
+// var android = { name: "Android", price: 499 };
+// var windowsPhone = { name: "Windows Phone", price: 399 };
+
+// var phoneStore = { name: "RadioShack", products: [iPhone, android, windowsPhone] };
+
+// var stores = [groceryStore, phoneStore];
+
+// stores.forEach(function(store) {
+//   console.log(store.name + " sells:");
+//   store.products.forEach(function(product) {
+//     console.log(product.name);
+//   });
+//   console.log("\n");
+// });    
